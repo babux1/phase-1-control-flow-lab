656ccf31d270de04ccd9c9801724b2797ae9eebb
batmunkh namshir <namshir17@gmail.com>
2022-01-10 19:25:04 -0800

    commit tree 680d987c3a0d2569c0a2175c9563bafb32860a34

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (2000 < ride && ride <2500){
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+  // Write your code here!
+function ternaryCheckCity(city){
+return (city === "NYC") ? ("Ok, sounds good.") : ("No go.");
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+switch (tip) {
+  case "generous":
+    return 'Thank you so much.';
+    case "not as generous":
+      return "Thank you.";
+      default :
+      return "Bye.";
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
